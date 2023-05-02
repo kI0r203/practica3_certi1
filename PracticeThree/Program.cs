989f015e24d2a8bc893701a1bdfff6216e1c364f
Nicolas Vargas <nicovarojas@gmail.com>
2023-05-02 15:11:36 -0400

    P3-001 Configured for serilog

--- PracticeThree/Program.cs
+++ PracticeThree/Program.cs
@@ -1,6 +1,14 @@
 using Microsoft.OpenApi.Models;
+using Serilog;
+
+//create the logger and setup your sinks, filters and properties
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog();
 
 // Add services to the container.
 
@@ -32,6 +40,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsEnvironment(builder.Environment.EnvironmentName))
 {
+    Log.Information("Ejecutando en el ambiente de " + builder.Environment.EnvironmentName);
     app.UseSwagger();
     app.UseSwaggerUI();
 }
